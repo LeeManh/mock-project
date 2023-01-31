5f958ade5e601e6b22f35d44d3a4a57d25e0379d
ManhLV <lemanhddt@gmail.com>
2023-02-01 05:35:02 +0700

    logic filter sort

--- src/types/product.type.ts
+++ src/types/product.type.ts
@@ -2,7 +2,7 @@ export interface ProductListConfig {
   page?: string
   limit?: string
   order?: 'desc' | 'asc'
-  sort_by?: 'createdAt' | 'view' | 'sold' | 'price'
+  sort_by?: 'ctime' | 'sales' | 'price'
   exclude?: string
   rating_filter?: number
   price_max?: string
